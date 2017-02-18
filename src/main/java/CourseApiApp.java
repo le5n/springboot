5fcf3bcc6570b67e577e4e78ba937d4d6f543038
Le5n <ellenageor@gmail.com>
2017-02-18 14:14:10 +0300

    Initial commit

---
+++ src/main/java/CourseApiApp.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args) {
+        
+    }
+}
