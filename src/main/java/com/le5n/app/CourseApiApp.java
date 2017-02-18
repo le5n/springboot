61023c0c13befa2e31fe10e5c031e2b719200f73
Le5n <ellenageor@gmail.com>
2017-02-18 14:36:25 +0300

    working empty app

--- src/main/java/com/le5n/app/CourseApiApp.java
+++ src/main/java/com/le5n/app/CourseApiApp.java
@@ -1,8 +1,11 @@
+package com.le5n.app;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseApiApp {
     public static void main(String[] args) {
-        
+        SpringApplication.run(CourseApiApp.class,args);
     }
 }
